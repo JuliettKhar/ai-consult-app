50afcbcb42cc77f375defe33db555168ab3ddf3d
julia kharlamova <juliettkhar@gmail.com>
2025-07-10 11:04:23 +0900

    add sessions endpoint

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,6 +22,12 @@ const router = createRouter({
       component: () => import('../views/FAQ.vue'),
       props: true,
     },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: () => import('../views/Admin.vue'),
+      props: true,
+    },
   ],
   scrollBehavior() {
     return { top: 0 }

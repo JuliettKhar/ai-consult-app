3d3d84b3ba6502332abc26381de6d47a763c7aed
julia kharlamova <juliettkhar@gmail.com>
2025-07-16 09:23:51 +0900

    update config to amplify deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ai-consult-app/',
+  base: '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
